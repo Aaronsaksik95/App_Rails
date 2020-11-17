2e7ae18d9d6c4038e3c0206e873a58fb6b78207f
Aaron Saksik <aaron.saksik@ynov.com>
2020-11-17 18:05:48 +0100

    task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_17_155333) do
+ActiveRecord::Schema.define(version: 2020_11_17_163406) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -19,4 +27,5 @@ ActiveRecord::Schema.define(version: 2020_11_17_155333) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "tasks", "users"
 end
